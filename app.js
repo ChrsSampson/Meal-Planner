572317d9e7f52dea6a9d8fa78abaa1e7bdad74b9
Chris Sampson <65880029+JorganBonstragel@users.noreply.github.com>
2020-12-18 16:11:35 -0500

    Functionality Complete

--- app.js
+++ app.js
@@ -1,15 +1,61 @@
-// VARIABLES
+// Selectors
 const main = document.querySelector('main');
 const headline = document.querySelector('.headline');
 const tagline = document.querySelector('.tagline');
 const btnContainer = document.getElementById('btn-container');
 const startButton = document.getElementById('startButton');
+const loginButton = document.getElementById('loginButton');
 const newButton = document.getElementById('newUser');
-const userList = document.getElementById('userList');
+const resetButton = document.getElementById('resetButton');
+const userList = document.getElementById('users');
 const planDiv = document.querySelector('.plan');
 const mealList = document.getElementById('meals');
-const name = ""; // Assign your name
-const meals = []; // Add in some meals...
+
+// Variables
+
+let user = ""; 
+const meals = [
+    "Honey Garlic Glazed Salmon",
+    "Chicken Stew",
+    "Chicken Posole",
+    "Bacon & Spinach Stuffed Chicken",
+    "Tex-Mex Meatballs",
+    "Easy Turkey Tetrazzini",
+    "Air Fryer Pork Chops",
+    "Easy Chicken Fajitas",
+    "Classic Calzones",
+    "Baked Spaghetti",
+    "Cilantro Lime Chicken",
+    "Creamy Tuscan Chicken",
+    "Garlicky Spaghetti",
+    "Turkey Casserole",
+    "Cheesy Ground Beef Tacos",
+    "Better-Than-Takeout Beef and Broccoli",
+    "Crock-Pot Chicken Noodle Soup",
+    "Thai Chicken Lettuce Cups",
+    "Air Fryer Shrimp",
+    "Lemon Pepper Chicken",
+    "Sheet Pan Honey Mustard Pretzel Chicken",
+    "Beef Totchos",
+    "Pineapple Baked Chicken",
+    "Easy Pasta Carbonara",
+    "Slow-Cooker Chicken Tortilla Soup",
+    "Easy Shepherd's Pie",
+    "Slow Cooker Chicken Thighs",
+    "Easy Cajun Jambalaya",
+    "Italian Chicken Skewers",
+    "Sloppy Joe Grilled Cheese",
+    "Chicken 'n Waffles Casserole",
+    "A Single Tater-Tot",
+    "The blood of your enemies",
+    "Revenge (Served cold)",
+    "A Single Crouton",
+    "Tater Salad",
+    "Sleep",
+    "Hot BBQ Chicken Wings",
+    "Organic Low Fat Sugar Free Diet Kosher Vegan Never-Frozen 'Turkey' Slab",
+    "Avacado Toast"
+    ]; 
 
 // HELPER FUNCTIONS
 
@@ -29,54 +75,147 @@ const getDay = () => {
     return weekday[day];
 };
 
-// create new users and keep track of user list
+// Create new users and keep track of user list in Local Storage
 function createUser(username){
-    let oldUsrs = localStorage.getItem('users');
-    
-    let newUsr = username;
+    let newUsr = ''
 
-    oldUsrs.push(newUsr);
+    if(!localStorage.getItem('users') == null){
 
-   localStorage.setItem('users', oldUsrs);
+        console.error("local storage blank")
+
+        let oldUsrs = localStorage.getItem('users')
+
+        oldUsrs = JSON.parse(oldUsrs);
+    }
+    // Check input for not blank
+    else if(username != ""){
+        let oldUsrs = localStorage.getItem('users')
+        oldUsrs = JSON.parse(oldUsrs);
+
+        newUsr = username;
+        
+        oldUsrs.push(newUsr);
+
+        console.log(oldUsrs);
+
+        localStorage.setItem('users', JSON.stringify(oldUsrs));
+        user = username
+
+        showStartScreen(user, getDay());
+    }
+    // Error Path
+    else{
+       alert("No Name entered");
+       resetApp();
+    }
+      
 }
 
-// Compile user list and show users
+// Compile user list and show list of users
 function showUserList(){
-    localStorage.forEach((username, i) => {
-        let usr = localStorage.getItem(username[i]);
-        let listItem = document.createElement('li');
-        listItem.textContent = usr;
-        userList.appendChild(listItem);
-    });
-    userList.classList.remove(hidden);
+    userList.innerHTML = "";
+
+    let storedUsers = localStorage.getItem('users');
+    storedUsers = JSON.parse(storedUsers);
+    console.log(storedUsers)
+
+    for(let i = 0; i < storedUsers.length; i++){
+        let item  = document.createElement('li');
+        item.textContent = storedUsers[i];
+        userList.appendChild(item);
+    }
+
+    planDiv.classList.remove('hidden');
+    userList.classList.remove('hidden');
 }
 
-const getRandomWeeklyMeals = () => {};
+// Hide account buttons and show start button with username displayed
+function showStartScreen(user, day){
+    headline.textContent = `Welcome ${user}`
+    tagline.textContent = `Get ready to view some of your options for ${day}`
+    loginButton.classList.add("hidden");
+    newButton.classList.add("hidden");
+    startButton.classList.remove("hidden")
+}
 
-const displayWeeklyMealPlan = () => {};
+// Generate Random Numbers and use them on the meals array.
+function selectMeals(max){
+    let randomMeals = [];
+    let selectedMeals = [];
+    for(let i = 0; i < 5; i++){
+       let arrItem = Math.floor(Math.random() * Math.floor(max));
+        randomMeals.push(arrItem);
+    }
+    
+    for(let i = 0; i < randomMeals.length; i++){
+        selectedMeals.push(meals[randomMeals[i]]);
+    }
+    
+    return selectedMeals;
 
-const showPlan = () => {};
+}
 
+// Generate Meal List from Selected Meals
+function displayMeals(mealArr){
+    for(let i = 0; i < mealArr.length; i++ ){
+        let item = document.createElement('li');
+        item.textContent = mealArr[i];
+        mealList.appendChild(item);
+    }
+}
 
-headline.textContent = getDay();
-tagline.textContent = `Hey ${name}, click the button below to see your weekly meal planner.`;
+// Display meals To user 
+function showPlan(){
+    mealList.innerHTML = "";
+
+    let userMeals = selectMeals(meals.length);
+    displayMeals(userMeals);
+
+    planDiv.classList.remove('hidden');
+    mealList.classList.remove('hidden');
+    startButton.textContent = `Show other options`;
+}
+
+function resetApp(){
+    mealList.innerHTML = "";
+    headline.textContent = `Who Needs Food?`;
+    tagline.textContent = `Select or Enter Another User`;
+    resetButton.classList.add('hidden');
+    startButton.classList.add('hidden');
+    loginButton.classList.remove('hidden');
+    newButton.classList.remove('hidden')
+    userList.classList.add('hidden');
+    planDiv.classList.add('hidden');
+}
+
 
 // EVENT LISTENERS
+
+//Buttons 
 btnContainer.addEventListener('click', (e) => {
     if(e.target.id == "newUser"){
-        console.info("new user")
         createUser(prompt('Please Enter your name'));
-
+    }
+    else if(e.target.id == "loginButton"){
+        showUserList();
+        resetButton.classList.remove('hidden');
+        newButton.classList.add('hidden');
     }
     else if(e.target.id == "startButton"){
-        console.info("Log in")
-        showUserList();
+        showPlan();
+        resetButton.classList.remove('hidden')
     }
+    else if(e.target.id == "resetButton"){
+        resetApp()
+    }
+
 });
 
+// User Select
 userList.onclick = (e) => {
-    name = e.target.textContent;
-    userList.classList.add(hidden);
+    user = e.target.textContent;
+    userList.classList.add('hidden');
+    showStartScreen(user, getDay());
 }
 
 
